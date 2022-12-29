234721fa5d33ded3521e30d655b6edffd99cba35
igknighton <trent.knighton@gmail.com>
2022-12-28 18:34:44 -0600

    Search toggle implementation; searching by region/category implementation

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,11 +1,19 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import RecipeThumbnail from "./RecipeThumbnail";
 import useApi from "../hooks/useApi";
+import {logDOM} from "@testing-library/react";
 const Recipes = () => {
 
     const [recipes,setRecipes] = useState([]);
+    const [searchOption,setSearchOption] = useState(2);
+        const {
+        searchMeals,
+        searchRegion,
+        searchCategory,
+        listCategories,
+        listRegions
+    } = useApi();
 
-    const {searchMeals} = useApi();
     const handleSearch = async e => {
         try {
             e.preventDefault();
@@ -17,12 +25,99 @@ const Recipes = () => {
         }
     }
 
+
+    const [categories, setCategories] = useState([]);
+    const [selectedCat,setSelectedCat] = useState('')
+    const handleCategory = e => setSelectedCat(e.target.value);
+
+    const searchByCategory = async e => {
+        try {
+            e.preventDefault();
+            const meals = await searchCategory(selectedCat);
+            setRecipes(meals);
+        } catch (e) {
+            console.error('An Error Occurred', e);
+        }
+    }
+
+    useEffect(() => {
+        listCategories().then(
+            categories => setCategories(categories)
+        );
+    }, []);
+
+
+    const KeywordSearch = () => {
+        return <form className={'searchBar'} onSubmit={handleSearch}>
+            <input type="text"/>
+            <input type="submit" value={'Search'}/>
+        </form>
+    }
+
+    const [regions,setRegions] = useState([]);
+    const [selectedRegion,setSelectedRegion] = useState('');
+
+    const handleRegion = e => setSelectedRegion(e.target.value);
+
+    const searchByRegion = async e => {
+        try {
+            e.preventDefault();
+            const meals = await searchRegion(selectedRegion);
+            setRecipes(meals);
+        } catch (e) {
+            console.error('An Error Occurred',e);
+        }
+    }
+
+    useEffect(() => {
+        listRegions().then(
+            regions => setRegions(regions)
+        )
+    },[]);
+
     return (
         <>
-            <form className={'searchBar'} onSubmit={handleSearch}>
-                <input type="text"/>
+            <div className="searchBy">
+                <h3>Select a search option: </h3>
+                <div className="searchByButtons">
+                    <div onClick={() =>setSearchOption(1)} className="searchByBtn">Keyword</div>
+                    <div onClick={() =>setSearchOption(3)} className="searchByBtn">Category</div>
+                    <div onClick={() =>setSearchOption(2)} className="searchByBtn">Region</div>
+                </div>
+            </div>
+            { searchOption === 1 && <KeywordSearch/> }
+            { searchOption === 2 && <form className={'searchBar'} onSubmit={searchByRegion}>
+                <label htmlFor="region">Region:</label>
+                <select name="region" id="region" onChange={handleRegion}>
+                    {
+                        regions.map(region => (
+                            <option
+                                value={region.strArea}
+                                key={region.strArea}>
+                                {region.strArea}
+                            </option>
+                        ))
+                    }
+                </select>
                 <input type="submit" value={'Search'}/>
             </form>
+            }
+            { searchOption === 3 && <form className={'searchBar'} onSubmit={searchByCategory}>
+                <label htmlFor="category">Category:</label>
+                <select name="category" id="category" onChange={handleCategory}>
+                    {
+                        categories.map(category => (
+                            <option
+                                value={category.strCategory}
+                                key={category.strCategory}>
+                                {category.strCategory}
+                            </option>
+                        ))
+                    }
+                </select>
+                <input type="submit" value={'Search'}/>
+            </form>
+            }
             <div className={'recipeContainer'}>
                 {
                     recipes === null ?
--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -16,9 +16,34 @@ const useApi = () => {
         return await api(`lookup.php?i=${mealId}`);
     }
 
+
+    const searchCategory = async category => {
+        const res = await api(`filter.php?c=${category}`)
+        return res.data.meals;
+    }
+
+    const searchRegion = async region => {
+        const res = await api(`filter.php?a=${region}`);
+        return res.data.meals;
+    }
+
+    const listCategories = async () => {
+        const res = await api(`list.php?c=list`);
+        return res.data.meals;
+    }
+
+    const listRegions = async () => {
+        const res = await api(`list.php?a=list`);
+        return res.data.meals;
+    }
+
     return {
         searchMeals,
-        loadMeal
+        loadMeal,
+        searchCategory,
+        searchRegion,
+        listRegions,
+        listCategories
     };
 };
 
